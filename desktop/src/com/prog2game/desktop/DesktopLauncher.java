9b65d110f3230279e14fa4c45a927106faa35cff
Nicolai Skye <d4rkeclipse@gmail.com>
2020-06-04 23:11:02 +0700

    Added window title

--- desktop/src/com/prog2game/desktop/DesktopLauncher.java
+++ desktop/src/com/prog2game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.prog2game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("No Man's Tower");
 		config.setResizable(false);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
